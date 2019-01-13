a44fea9a991b9e579f06a3fab89ee4ec8d40bd75
Nabeel Siddiqui <n.siddiqui25@gmail.com>
2019-01-13 02:30:13 -0600

    install mixins plugin - change hero css to incorporate mobile-first

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,13 @@ postCSS = require('gulp-postcss'),
 autoPrefixer = require('autoprefixer'),
 cssVars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 
 gulp.task('styles', function(){
    return gulp.src('./app/assets/styles/style.css')
-      .pipe(postCSS([cssImport, cssVars, nested, autoPrefixer]))
+      .pipe(postCSS([cssImport, mixins, cssVars, nested, autoPrefixer]))
       .on('error', function(errorInfo){
          console.log(errorInfo.toString());
          this.emit('end');
